d18f69a6bb899470dd415f6803f5e8660442f3ec
Birdmanbytheclaw <therealparkerflynn@gmail.com>
2020-01-22 11:16:48 -0600

    Finally figured out what was going wrong and can now start writing tests

---
+++ LemonadeStandTesting/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LemonadeStand;
+namespace LemonadeStandTesting
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Game game = new Game();
+        }
+    }
+}
